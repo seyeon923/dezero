ee1da017a770162e01055e83b2ad8e6be5002a66
Kim Se-yeon <tpdussla93@gmail.com>
2022-07-15 17:00:34 +0900

    Add functions.Exp derived from Function

--- dezero/functions.py
+++ dezero/functions.py
@@ -8,6 +8,18 @@ class Square(Function):
         return x ** 2
 
 
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
 if __name__ == '__main__':
     square = Square()
+    exp = Exp()
+
     assert square(Variable(np.array(10))).data == 100
+    assert np.isclose(exp(Variable(np.array(1))).data, np.exp(1))
+
+    x = Variable(np.array(0.5))
+    y = square(exp(square(x)))
+    assert np.isclose(y.data, np.exp(0.25) * np.exp(0.25))
