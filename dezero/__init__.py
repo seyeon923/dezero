ded03554c1ab1d145c5fa2d1f3755add3f2981ac
Kim Se-yeon <tpdussla93@gmail.com>
2022-07-16 18:06:36 +0900

    Add auto diff function

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -4,6 +4,18 @@ __all__ = ['Variable', 'Function', 'functions']
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        creator = self.creator
+
+        if creator is not None:
+            creator.input.grad = creator.backward(self.grad)
+            creator.input.backward()
 
 
 class Function:
@@ -13,7 +25,13 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
+        self.input = input
+        self.output = output
         return output
 
     def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
         raise NotImplementedError()
--- dezero/functions.py
+++ dezero/functions.py
@@ -7,10 +7,20 @@ class Square(Function):
     def forward(self, x):
         return x ** 2
 
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 
 if __name__ == '__main__':
@@ -23,3 +33,21 @@ if __name__ == '__main__':
     x = Variable(np.array(0.5))
     y = square(exp(square(x)))
     assert np.isclose(y.data, np.exp(0.25) * np.exp(0.25))
+
+    A = Square()
+    B = Exp()
+    C = Square()
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    assert y.creator == C
+    assert C.input == b
+    assert b.creator == B
+    assert B.input == a
+    assert a.creator == A
+    assert A.input == x
+
+    y.grad = np.array(1.)
+    y.backward()
+    print(x.grad)
