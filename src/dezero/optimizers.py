20933a06776b68eec44604ffffbd2601c6a85516
Kim Se-yeon <tpdussla93@gmail.com>
2023-03-30 16:56:25 +0900

    Add MomentumSGD(Optimizer) to optimizers

--- src/dezero/optimizers.py
+++ src/dezero/optimizers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from . import Layer, Variable
 
 
@@ -36,3 +38,23 @@ class SGD(Optimizer):
 
     def update_one(self, param: Variable):
         param.data -= self.lr * param.grad.data
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+
+        self.lr = lr
+        self.momentum = momentum
+        self.__vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.__vs:
+            self.__vs[v_key] = np.zeros_like(param.data)
+
+        v = self.__vs[v_key]
+
+        v *= self.momentum
+        v -= self.lr * param.grad.data
+        param.data += v
--- tests/simple_nonlinear_fit.py
+++ tests/simple_nonlinear_fit.py
@@ -8,26 +8,13 @@ y = np.sin(2*np.pi * x) + np.random.rand(100, 1)
 x, y = dz.Variable(x), dz.Variable(y)
 
 
-class TwoLayerModel(dz.Model):
-    def __init__(self, hidden_size, out_size):
-        super().__init__()
-
-        self.l1 = layers.Linear(hidden_size)
-        self.l2 = layers.Linear(out_size)
-
-    def forward(self, x):
-        y = self.l1(x)
-        y = functions.sigmoid(y)
-        return self.l2(y)
-
-
 model = models.MLP((10, 1))
 model.plot(x)
 
 lr = 0.2
 iters = 10000
 
-optimizer = optimizers.SGD(lr).setup(model)
+optimizer = optimizers.MomentumSGD(lr).setup(model)
 
 for i in range(iters):
     y_pred = model(x)
