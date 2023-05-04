2a6405e3e007e576398db5373cfbd438ab3bc0cc
Kim Se-yeon <tpdussla93@gmail.com>
2023-05-04 15:29:08 +0900

    Add dataset, transforms module and dependent functions to utils

--- src/dezero/utils.py
+++ src/dezero/utils.py
@@ -1,10 +1,12 @@
-from string import Template
-from io import StringIO
 import tempfile
 import os
 import traceback
 import sys
 import subprocess
+import urllib
+
+from string import Template
+from io import StringIO
 
 import numpy as np
 
@@ -110,6 +112,69 @@ def sum_to(x: np.ndarray, shape) -> np.ndarray:
     if lead > 0:
         y = y.squeeze(lead_axis)
     return y
+
+
+# =============================================================================
+# download function
+# Copied from https://github.com/oreilly-japan/deep-learning-from-scratch-3/blob/master/dezero/utils.py
+# =============================================================================
+def show_progress(block_num, block_size, total_size):
+    bar_template = "\r[{}] {:.2f}%"
+
+    downloaded = block_num * block_size
+    p = downloaded / total_size * 100
+    i = int(downloaded / total_size * 30)
+    if p >= 100.0:
+        p = 100.0
+    if i >= 30:
+        i = 30
+    bar = "#" * i + "." * (30 - i)
+    print(bar_template.format(bar, p), end='')
+
+
+cache_dir = os.path.join(os.path.expanduser('~'), '.dezero')
+
+
+def get_file(url, file_name=None):
+    """Download a file from the `url` if it is not in the cache.
+    The file at the `url` is downloaded to the `~/.dezero`.
+    Args:
+        url (str): URL of the file.
+        file_name (str): Name of the file. It `None` is specified the original
+            file name is used.
+    Returns:
+        str: Absolute path to the saved file.
+    """
+    if file_name is None:
+        file_name = url[url.rfind('/') + 1:]
+    file_path = os.path.join(cache_dir, file_name)
+
+    if not os.path.exists(cache_dir):
+        os.mkdir(cache_dir)
+
+    if os.path.exists(file_path):
+        return file_path
+
+    print("Downloading: " + file_name)
+    try:
+        urllib.request.urlretrieve(url, file_path, show_progress)
+    except (Exception, KeyboardInterrupt) as e:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        raise
+    print(" Done")
+
+    return file_path
+
+
+def pair(x):
+    if isinstance(x, int):
+        return (x, x)
+    elif isinstance(x, tuple):
+        assert len(x) == 2
+        return x
+    else:
+        raise ValueError
 
 
 if __name__ == '__main__':
